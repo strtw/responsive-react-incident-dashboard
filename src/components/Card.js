090f80d48c9356d395f87d1cf9ce94c2254c42df
strtw <souporstu@gmail.com>
2020-04-02 12:24:19 -0700

    Add Card component and basic styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,11 @@
 import React, {Component} from 'react';
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <h6 className="card__title">{props.title}</h6>
+            <hr/>
+            <h2 className="card__value">{props.value}</h2>
+        </div>
+    )
+}
