d5246606e4786726d40e8bc4c6eee9adf2cc0375
strtw <souporstu@gmail.com>
2020-04-03 12:41:31 -0700

    fixed className bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card(props){
     return(
         <div className="card" onClick={props.onClick}>
             <h6 className="card__title">{props.title}</h6>
-            <hr class="card__divider"/>
+            <hr className="card__divider"/>
             <h2 className="card__value">{props.value}</h2>
         </div>
     )
