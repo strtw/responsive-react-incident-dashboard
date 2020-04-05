b03a5c7e7cf5d734e08a9d8f380f4f817e7b21cf
strtw <souporstu@gmail.com>
2020-04-04 17:07:07 -0700

    Save devDependency for Prettier and ESLint & test pre-commit hook

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Card(props){
-    return(
-        <Link to={props.to} style={props.linkStyle ? props.linkStyle : {textDecoration: 'none', color: 'inherit'}}>
-            <div className={props.filteredBy === props.title ? "card card--active": "card"} onClick={props.onClick} style={props.width?{width:props.width} : {width:'100%'}}>
-                <h6 className="card__title">{props.title}</h6>
-                <hr className="card__divider"/>
-                <h2 className="card__value">{props.value}</h2>
-            </div>
-       </Link>
-    )
+export default function Card(props) {
+  return (
+    <Link
+      to={props.to}
+      style={
+        props.linkStyle
+          ? props.linkStyle
+          : { textDecoration: "none", color: "inherit" }
+      }
+    >
+      <div
+        className={
+          props.filteredBy === props.title ? "card card--active" : "card"
+        }
+        onClick={props.onClick}
+        style={props.width ? { width: props.width } : { width: "100%" }}
+      >
+        <h6 className="card__title">{props.title}</h6>
+        <hr className="card__divider" />
+        <h2 className="card__value">{props.value}</h2>
+      </div>
+    </Link>
+  );
 }
-
-console.log("test");
